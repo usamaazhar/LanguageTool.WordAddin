85b201d99ba37c5412fb07406586f440ccba850d
Usama.Azhar <Usama.Azhar@ASIA13921>
2017-08-08 10:35:29 +0500

    changed assembly name and installer

--- LanguageTool.WordAddin/Properties/AssemblyInfo.cs
+++ LanguageTool.WordAddin/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Security;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("LanguageTool.WordAddin")]
+[assembly: AssemblyTitle("The Pronet Word Add In")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("LanguageTool.WordAddin")]
+[assembly: AssemblyProduct("The Pronet Word Add In")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
